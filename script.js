e21f6ae33517b9dd97105c6cd3e32d675fe65dbc
Alviansyah Razak <50584676+alviansyahgithub@users.noreply.github.com>
2021-10-18 21:03:52 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,71 @@
+//Navbar
+const mainMenu = document.querySelector(".mainMenu");
+const closeMenu = document.querySelector(".closeMenu");
+const openMenu = document.querySelector(".openMenu");
+
+openMenu.addEventListener("click", show);
+closeMenu.addEventListener("click", close);
+
+function show() {
+  mainMenu.style.display = "flex";
+  mainMenu.style.top = "0";
+}
+function close() {
+  mainMenu.style.top = "-100%";
+}
+
+//Hero
+window.addEventListener("scroll", function () {
+  let header = document.querySelector("header");
+  let windowPosition = window.scrollY > 0;
+  header.classList.toggle("scrolling-active", windowPosition);
+});
+
+//practice
+let slideIndex = 1;
+
+const plusSlides = (n) => {
+  slideIndex += n;
+  showSlides(slideIndex);
+};
+
+const showSlides = (n) => {
+  const slides = document.getElementsByClassName("mySlides");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex - 1].style.display = "block";
+};
+
+showSlides(slideIndex);
+
+//team
+let popupViews = document.querySelectorAll(".popup-view");
+let popupBtns = document.querySelectorAll(".popup-btn");
+let closeBtns = document.querySelectorAll(".close-btn");
+
+//javascript for quick view button
+let popup = function (popupClick) {
+  popupViews[popupClick].classList.add("active");
+};
+
+popupBtns.forEach((popupBtn, i) => {
+  popupBtn.addEventListener("click", () => {
+    popup(i);
+  });
+});
+
+//javascript for close button
+closeBtns.forEach((closeBtn) => {
+  closeBtn.addEventListener("click", () => {
+    popupViews.forEach((popupView) => {
+      popupView.classList.remove("active");
+    });
+  });
+});
